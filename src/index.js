8d525f23b45c8e2c763612cc674f16ef648b8f73
Svetlana <brigantina@mail.ru>
2025-04-29 20:17:49 +0300

    Update index.js
    fixed function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  if (number >=1 && number <=999)
+  throw new Error('Number must be between of 1 and 97');
 };
