f1ef43369ade5576bd50a88756662f7638c535de
Svetlana <brigantina@mail.ru>
2025-04-29 20:57:06 +0300

    Update index.js
    fix function

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ module.exports = function toReadable(number) {
   if (number < 0 && number > 999)
   throw new Error('Number must be between of 1 and 999');
 };
-const ones = [];
+const ones = ['zero', 'one', 'two', 'three', 'four', 'five',
+    'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen',
+    'fifteen', 'sixteen', 'seventeen', 'eighteen',
+    'nineteen'];
 const tens = [];
 const hundreds = [];
 
-if (numbers < 10) {
+if (numbers < 20) {
   return ones
 }
 else if (numbers > 10) {
    return tens
 }
+
+else {}
 
