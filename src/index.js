9ba1dac35d4de30a2728cb1200be8010fe68723b
Svetlana <brigantina@mail.ru>
2025-04-29 20:31:33 +0300

    Update index.js
    add const and if

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 module.exports = function toReadable(number) {
-  if (number >=1 && number <=999)
-  throw new Error('Number must be between of 1 and 97');
+  if (number < 0 && number > 999)
+  throw new Error('Number must be between of 1 and 999');
 };
+const ones = [];
+const tens = [];
+const hundreds = [];
+
+if (numbers < 10) {
+  return ones
+}
+else if (numbers > 10) {
+   return tens
+}
+
