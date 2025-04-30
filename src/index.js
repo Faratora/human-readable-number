84c173ed696de95dd7e1a777a379ba4b11405795
Svetlana <brigantina@mail.ru>
2025-04-30 22:15:13 +0300

    Update index.js
    fix function

--- src/index.js
+++ src/index.js
@@ -1,31 +1,73 @@
 module.exports = function toReadable(number) {
-  if (number < 0 || number > 999){
-  throw new Error('Number must be between of 1 and 999');
-}
-const ones = ['zero', 'one', 'two', 'three', 'four', 'five',
-    'six', 'seven', 'eight', 'nine', 'ten',
-    'eleven', 'twelve', 'thirteen', 'fourteen',
-    'fifteen', 'sixteen', 'seventeen', 'eighteen',
-    'nineteen'];
-const tens = ['twenty', 'thirty', 'forty',
-    'fifty', 'sixty', 'seventy', 'eighty',
-    'ninety'];
-const hundreds = ['one hundred', 'two hundred', 'three hundred',
-    'four hundred', 'five hundred', 'six hundred',
-    'seven hundred', 'eight hundred', 'nine hundred'];
+  if (number < 0 || number > 999) {
+    throw new Error('Number must be between of 1 and 999');
+  }
 
-    if (number < 20) {
-      word = ones[number];
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  const hundreds = [
+    'one hundred',
+    'two hundred',
+    'three hundred',
+    'four hundred',
+    'five hundred',
+    'six hundred',
+    'seven hundred',
+    'eight hundred',
+    'nine hundred',
+  ];
+
+  let numbers = '';
+
+  if (number < 20) {
+    numbers = ones[number];
   } else if (number < 100) {
-     word = tens[Math.floor(number / 10)] + (number % 10 ? ' ' + ones[number % 10] : '');
+    numbers =
+      tens[Math.floor(number / 10) - 2] +
+      (number % 10 ? ` ${ones[number % 10]}` : '');
   } else {
-      let hundredPart = hundreds[Math.floor(number / 100) - 1];
-      let remainder = number % 100;
+    const hundredPart = hundreds[Math.floor(number / 100) - 1];
+    const remainder = number % 100;
 
-      if (remainder === 0) {
-        word =  hundredPart;
-      } else {
-        word =  hundredPart + ' ' + toReadable(remainder);
-      }
-  };
-  console.log(toReadable(10));
+    if (remainder === 0) {
+      numbers = hundredPart;
+    } else {
+      numbers = `${hundredPart} ${toReadable(remainder)}`;
+    }
+  }
+
+  return numbers;
+};
+console.log(toReadable(140));
